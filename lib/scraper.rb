eaa40a7f3032b8cc2dc0d9ac0991f59c438e71ea
Timur Mishiev <timurmishiev@gmail.com>
2020-03-03 23:52:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students = []
+    
     html = open(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.student-card").each do |student|
